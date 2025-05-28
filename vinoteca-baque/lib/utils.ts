52f394b9a0e54bc4f0a0655be5169e0a342ed835
Gerson Baque <gersonbaque19@gmail.com>
2025-05-28 13:42:30 +0200

    Función de fecha

--- vinoteca-baque/lib/utils.ts
+++ vinoteca-baque/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+ 
+export function dateFormat(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  return new Date(date).toLocaleDateString('es-ES', options)
+}
